f74bf96b31d4596237e78f1380ae8c9eb71cf451
ananastea <43419371+ananastea@users.noreply.github.com>
2018-12-04 08:51:22 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -230,12 +230,12 @@ function sentFormData() {
 		const xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
         xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-        xhr.send(formData);
+        xhr.send(JSON.stringify(formData));
 		const overlay = createOverlay(document.querySelector('#overlayTemplate').innerHTML);
 		xhr.addEventListener('load', () => {
 			overlay.open();
 			if (xhr.status <= 400) {
-				const message = xhr.response.message;
+				const message = 'Заказ оформлен';
 				overlay.setContent('', message);
 			} else {
 				const message = 'Ошибка! ';
@@ -269,5 +269,320 @@ function reviewsOverlay() {
 reviewsOverlay();
 
 
+function onePageScroll() {
+  const wrapper = document.querySelector('.wrapper');
+  const content = wrapper.querySelector('.maincontent');
+  const pages = content.querySelectorAll('.section');
+  const points = document.querySelectorAll('.point__item');
+  const dataScrollto = document.querySelectorAll('[data-scroll-to]');
+
+  let isScroll = false;
+
+  addNavigation();
+  wheel();
+  keyPush();
+
+  function moveToPage(numPage) {
+    const position = `${numPage * -100}%`;
+
+    if (isScroll) return;
+
+    isScroll = true;
+
+    addClass(pages);
+		setTimeout(() => {
+      isScroll = false;
+      addClass(points);
+    }, 1000);
+    content.style.transform = `translateY(${position})`;
+		function addClass(arr) {
+      arr[numPage].classList.add('is-active');
+      for (const iterator of arr) {
+        if (iterator !== arr[numPage]) {
+          iterator.classList.remove('is-active');
+        }
+      }
+    }
+  }
+  
+
+  function addNavigation() {
+    for (const iterator of dataScrollto) {
+      iterator.addEventListener('click', e => {
+        e.preventDefault();
+        moveToPage(iterator.dataset.scrollTo);
+      });
+    }
+  }
+
+  function wheel() {
+    document.addEventListener('wheel', e => {
+      const direct = e.deltaY > 0 ? 'up' : 'down';
+
+      scrollToPage(direct);
+    });
+  }
+
+  function definePage(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      if (element.classList.contains('is-active')) {
+        return {
+          elIndex: i,
+          elActive: element,
+          elNext: element.nextElementSibling,
+          elPrev: element.previousElementSibling
+        };
+      }
+    }
+  }
+
+  function scrollToPage(direct) {
+    let page = definePage(pages);
+
+    if (direct === 'up' && page.elNext) {
+      let numPage = page.elIndex + 1;
+      moveToPage(numPage);
+    }
+    if (direct === 'down' && page.elPrev) {
+      let numPage = page.elIndex - 1;
+      moveToPage(numPage);
+    }
+  }
+
+  function keyPush() {
+    document.addEventListener('keydown', e => {
+      console.log(e.keyCode);
+      switch (e.keyCode) {
+        case 40:
+          scrollToPage('up');
+          break;
+        case 38:
+          scrollToPage('down');
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  if (isMobileDevice()) swipe();
+
+  function swipe() {
+    let touchStartY = 0;
+    let touchEndY = 0;
+
+    document.addEventListener(
+      'touchstart',
+      e => {
+        touchStartY = e.changedTouches[0].screenY;
+      },
+      false
+    );
+
+    wrapper.addEventListener('touchmove', e => e.preventDefault());
+
+    document.addEventListener(
+      'touchend',
+      e => {
+        touchEndY = e.changedTouches[0].screenY;
+        let direct = swipeDirect();
+        scrollToPage(direct);
+      },
+      false
+    );
+
+    function swipeDirect() {
+      let dif = touchStartY - touchEndY;
+      if (dif > 100) {
+        return 'up';
+      } else if (dif < -100) {
+        return 'down';
+      }
+    }
+  }
+
+  function isMobileDevice() {
+    return (
+      typeof window.orientation !== 'undefined' ||
+      navigator.userAgent.indexOf('IEMobile') !== -1
+    );
+  }
+}
+
+onePageScroll();
+
+function yandexMap() {
+
+	ymaps.ready(init);
+
+	const placemarks = [
+			{ 
+					latitude: 59.97, 
+					longitude: 30.31, 
+					hintContent: 'Наш адрес',
+					balloonContent: 'улица Литераторов, 17',
+					
+			},
+			{
+					latitude: 59.89, 
+					longitude: 30.32, 
+					hintContent: 'Наш адрес',
+					balloonContent: 'Московский проспект, 100',
+			},
+			{
+					latitude: 59.93, 
+					longitude: 30.34, 
+					hintContent: 'Наш адрес',
+					balloonContent: 'Наб. реки Фонтанки, 56',
+			},
+			{
+				latitude: 59.93, 
+				longitude: 30.40, 
+				hintContent: 'Наш адрес',
+				balloonContent: 'Малоохтинский проспект, 61',
+		}
+	];
+
+			geoObjects = [];
+
+	function init() {
+			const map = new ymaps.Map('map', {
+					center: [59.94, 30.32],
+					zoom: 12,
+					controls: ['zoomControl'],
+					behaviors: ['drag'] 
+			});
+
+	for (let i = 0; i < placemarks.length; i++) {
+							geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], 
+							{
+									hintContent: placemarks[i].hintContent,
+									balloonContent: placemarks[i].balloonContent
+							},
+							{
+									iconLayout: 'default#image',
+									iconImageHref: 'img/map-marker.svg',
+									iconImageSize: [46, 57],
+									iconImageOffset: [-23, -57] 
+							});
+			}
+			
+			const clusterer = new ymaps.Clusterer({
+					clusterIcons: [
+							{
+									href:  'img/map-marker.svg',
+									size: [100, 100],
+									offset: [-50, -50]
+							}
+					],
+					clusterIconContentLayout: null
+			});
+
+			map.geoObjects.add(clusterer);
+			clusterer.add(geoObjects);
+	}
+}
+yandexMap();
 
 
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('yt-player', {
+			  width: '660',
+        height: '405',
+        videoId: 'M7lc1UVf-VE',
+        playerVars: {
+            controls: 0,
+            disablekb: 0,
+            modestbranding: 0,
+            rel: 0,
+            autoplay: 0,
+            showinfo: 0
+        },
+        events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+const vid = document.getElementById("myVideo");
+function myVideo() { 
+    vid.load();
+} 
+
+function onPlayerStateChange(event) {
+
+    switch(event.data) {
+        case 1:
+            $('.player__start').addClass('paused');
+            $('.player__wrapper').addClass('active');
+            break;
+        case 2:
+            $('.player__start').removeClass('paused');
+    }
+}
+
+function onPlayerReady() {
+    const duration = player.getDuration();
+    let interval;
+
+    $('.player').removeClass('hidden');
+    updateTimer();
+    clearInterval(interval);
+    interval = setInterval(() => {
+        const completed = player.getCurrentTime();
+        const percent = (completed / duration) * 100;
+
+        updateTimer();
+        changeButtonPosition(percent);
+
+    }, 1000);
+}
+
+function updateTimer() {
+    $('.player__duration-completed').text(formatTime(player.getCurrentTime()));
+    $('.player__duration-estimate').text(formatTime(player.getDuration()));
+}
+
+function formatTime(time) {
+    const roundTime = Math.round(time);
+
+    const minutes = Math.floor(roundTime / 60);
+    const seconds = roundTime - minutes * 60;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return minutes + ":" + formattedSeconds;
+}
+
+$('.player__start').on('click', e => {
+    const playerStatus = player.getPlayerState(); // 0 - ended, 1 - played, 2 - paused ...
+
+
+    if (playerStatus !== 1 ) {
+        player.playVideo();
+    } else {
+        player.pauseVideo();
+    }
+});
+
+$('.player__playback').on('click', e => {
+    const bar = $(e.currentTarget);
+    const newButtonPosition = e.pageX - bar.offset().left;
+    const clickedPercent = (newButtonPosition / bar.width()) * 100;
+    const newPlayerTime = (player.getDuration() / 100) * clickedPercent;
+
+    changeButtonPosition(clickedPercent);
+    player.seekTo(newPlayerTime);
+});
+
+$('.player__splash').on('click', e => {
+    player.playVideo();
+})
+
+function changeButtonPosition(percent) {
+    $('.player__playback-button').css({
+        left: `${percent}%`
+    });
+}
